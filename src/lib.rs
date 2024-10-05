eeb88c48bf2804b93f5afee0c0e75fa87068a508
Jonah Meijers <jonahmeijers97@gmail.com>
2024-10-05 16:35:16 +0200

    add more support

--- src/lib.rs
+++ src/lib.rs
@@ -185,6 +185,12 @@ impl From<&[&str]> for TableRow {
     }
 }
 
+impl<T: std::fmt::Display, const N: usize> From<&[&T; N]> for TableRow {
+    fn from(value: &[&T; N]) -> Self {
+        Self::new(value.iter().map(|v| v.to_string()).collect())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
